a43af71e2a0946cd8889d49e67eb2125a8d0b02a
James Donohoe <james.j.donohoe@outlook.com>
2017-03-31 14:13:59 +0100

    Change Update

--- app/src/main/java/ayy/shopping/cashdisplay/ChangeDisplay.java
+++ app/src/main/java/ayy/shopping/cashdisplay/ChangeDisplay.java
@@ -2,7 +2,6 @@ package ayy.shopping.cashdisplay;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -12,12 +11,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import ayy.shopping.R;
-import ayy.shopping.entering.EnterAmountActivity;
-import ayy.shopping.textreading.OcrCaptureActivity;
 
 /**
  * Created by acale on 27/03/2017.
@@ -29,22 +27,16 @@ public class ChangeDisplay extends FragmentActivity {
     ViewPager mViewPager;
     HashMap<Float, Integer> pics;
     ArrayList<Float> cash;
-    TextToSpeech tts;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_display);
-        tts = new TextToSpeech(ChangeDisplay.this, new TextToSpeech.OnInitListener() {
-            //Getting text from the button and storing it in s
-
-            @Override
-            public void onInit(int status) {
-                //Instead of using the text in the button we are using our own message
-                tts.speak("Please take your change", TextToSpeech.QUEUE_FLUSH, null);
-            }
-        });
 
         pics = new HashMap<>();
 
+        pics.put(0.05f, R.drawable.fivecent);
+        pics.put(0.10f, R.drawable.tencent);
+        pics.put(0.20f, R.drawable.twentycent);
         pics.put(0.50f, R.drawable.fiftycent);
         pics.put(1.0f, R.drawable.one);
         pics.put(2.0f, R.drawable.two);
@@ -59,12 +51,11 @@ public class ChangeDisplay extends FragmentActivity {
         Intent intent = getIntent();
         float chnge = intent.getFloatExtra("changemsg", 0.0f);
 
+        DecimalFormat df = new DecimalFormat("0.00");
 
+        String myChange = String.valueOf(df.format(chnge));
 
-
-        String myChange = String.valueOf(chnge);
         String[] convert = myChange.split("\\.");   // Splits the change into euro and cent integers
-
         int euro = Integer.parseInt(convert[0]);
         int cent = Integer.parseInt(convert[1]);
 
@@ -82,8 +73,10 @@ public class ChangeDisplay extends FragmentActivity {
                 euro -= num * coin[i];
 
                 float moneyValue = coin[i];
-                cash.add(moneyValue);
-
+                for( int j = 0; j < num; j++)
+                {
+                    cash.add(moneyValue);
+                }
             }
         }
         // Calculates least number of cent coins
@@ -92,13 +85,22 @@ public class ChangeDisplay extends FragmentActivity {
                 num2 = cent / cent_coin[i];
                 System.out.println(num2 + " x " + "0." + cent_coin[i] + "c");
                 cent -= num2 * cent_coin[i];
+
+                float centValue = (cent_coin[i]*0.01f);
+                //System.out.println("float cent val: " + centValue);
+
+                float cent_number = Float.parseFloat(df.format(centValue));
+                //System.out.println("The number is: " + number);
+
+                for( int j = 0; j < num2; j++)
+                {
+                    cash.add(cent_number);
+                }
+
+
             }
         }
-        /*
-        for(int j =0 ; j < 10 ; j++ ) {
-            cash.add(10f);
-        }
-        */
+
         mPagerAdapter = new ChangeDisplay.PagerAdapter(getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mPagerAdapter);
@@ -108,7 +110,6 @@ public class ChangeDisplay extends FragmentActivity {
             public void onClick(View v) {
                 if (mViewPager.getCurrentItem() < cash.size() - 1) {
                     mViewPager.setCurrentItem(mViewPager.getCurrentItem() + 1);
-
                 }
             }
         });
